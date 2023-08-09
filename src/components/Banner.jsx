ceef3bb20310f9af4d7f918bca64254ab2fe2e1f
mohdkamleen <kamleenmohd@gmail.com>
2023-08-09 15:19:33 +0530

    change header part

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,27 +5,21 @@ import '../css/components/Banner.css'
 function Banner() {
   return (
     <Carousel>
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h1> Emprowing Society through Scince </h1>
-          <p>Food Lab is one point solution for all food safety needs</p>
-          <Button variant='success'> Get Started</Button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h1> Emprowing Society through Scince </h1>
-          <p>Food Lab is one point solution for all food safety needs</p>
-          <Button variant='success'> Get Started</Button>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Carousel.Caption>
-          <h1> Emprowing Society through Scince </h1>
-          <p>Food Lab is one point solution for all food safety needs</p>
-          <Button variant='success'> Get Started</Button>
-        </Carousel.Caption>
-      </Carousel.Item>
+    <Carousel.Item>  
+        <h1 className='banner-title'> Emprowing Society through Scince </h1>
+        <p className='banner-desc'>Food Lab is one point solution for all food safety needs</p>
+        <Button className='banner-btn' variant='success'> Get Started</Button> 
+    </Carousel.Item> 
+      <Carousel.Item>  
+          <h1 className='banner-title'> Emprowing Society through Scince </h1>
+          <p className='banner-desc'>Food Lab is one point solution for all food safety needs</p>
+          <Button className='banner-btn' variant='success'> Get Started</Button> 
+      </Carousel.Item> 
+      <Carousel.Item>  
+          <h1 className='banner-title'> Emprowing Society through Scince </h1>
+          <p className='banner-desc'>Food Lab is one point solution for all food safety needs</p>
+          <Button className='banner-btn' variant='success'> Get Started</Button> 
+      </Carousel.Item> 
     </Carousel>
   );
 }
